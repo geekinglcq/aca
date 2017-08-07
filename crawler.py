9925ee7f9553c805af7f0d2161b8ba404d553dd9
lunar <lcqgeek@live.com>
2017-08-07 22:31:20 +0800

    update add search page info

--- crawler.py
+++ crawler.py
@@ -12,14 +12,28 @@ from bs4 import BeautifulSoup as bs
 def get_search_page(search_url):
     """
     Return the search results of the given searching url.
-    Including the info of results title and url
+    Including the info of results title, url, detail and if or not have fl(bool)
     """
     try:
         html = urlopen(search_url, timeout=5)
         bsObj = bs(html.read())
-        res = bsObj.findAll("h3", {"class": "r"})
-        res = [[x.a.text, x.a["href"]] for x in res]
-        return res
+        res = bsObj.findAll("div", {"class": "rc"})
+        ans = []
+        for i in res:
+            sample = []
+            sample.append(i.h3.a.text)
+            temp = get_true_url(i.h3.a["href"]):
+            if temp == None:
+                sample.append(i.h3.a["href"])
+            else:
+                sample.append(temp)
+            sample.append(i.find("span", {"class": "st"}).text)
+            if i.div.div.find("div", {"class": "slp f"}) == None:
+                sample.append(1)
+            else:
+                sample.append(0)
+            ans.append(sample)
+        return ans
 
     except Exception as e:
       print(e)
@@ -32,7 +46,7 @@ def get_true_url(url):
     try:
 
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko'}
-        dlurl = requests.get(url, headers=headers, timeout=0.1)
+        dlurl = requests.get(url, headers=headers, timeout=5)
         return dlurl.url
     except Exception as e:
         return None
