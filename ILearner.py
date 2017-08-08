e1cfc2d1aaea52727d4468b199c7e57eb2543e6b
legend <Legend94rz@gmail.com>
2017-08-08 11:44:20 +0800

    1. fix a bug in ILearner; 2. update task3

--- ILearner.py
+++ ILearner.py
@@ -10,10 +10,17 @@ class ILearner(object):
 class Svr(ILearner):
 
 	def __init__(self, kernel='rbf', degree=3, gamma='auto', coef0=0.0, tol=0.001, C=1.0, epsilon=0.1, shrinking=True, cache_size=200, verbose=False, max_iter=-1):
-		self.kernel=kernel
+		self.kernel = kernel
 		self.C = C
-		self.gamma=gamma
-		self.model = SVR(kernel=self.kernel,gamma=self.gamma,C=self.C)
+		self.gamma = gamma
+		self.coef0 = coef0
+		self.tol = tol
+		self.epsilon = epsilon
+		self.shrinking = shrinking
+		self.cache_size = cache_size
+		self.verbose = verbose
+		self.max_iter = max_iter
+		self.model = SVR(kernel=self.kernel, C=self.C, gamma=self.gamma, coef0=self.coef0, tol=self.tol, epsilon=self.epsilon, shrinking=self.shrinking, cache_size=self.cache_size, verbose=self.verbose , max_iter=self.max_iter)
 
 	def train(self, X, y):
 		self.model.fit(X,y)
--- task3.py
+++ task3.py
@@ -14,7 +14,13 @@ output3Path="F:\\比赛数据\\task3\\output3.txt"
 
 tempPath="F:\\比赛数据\\task3\\temp3.txt"
 
-def Test():
+# 特征参数
+timespan = 5	#每5年的被引数为一个特征
+earliest = 1936
+latest = 2016
+
+
+def test():
 	while True:
 		aut = raw_input("author Name:")
 		if aut=="-1":
@@ -36,6 +42,26 @@ def GroupReferedPaperByYear(author):
 			else:
 				res[refed.Time] = [refed]
 	return res,total
+
+def SortAutAndReferred(res):
+	keys=res.keys()
+	keys.sort()
+	ty=[len(res[k]) for k in keys]
+	return keys,ty
+
+def CompleteReferred(keys,ty):
+	time=keys[0]
+	i=0
+	while time<=latest:
+		if i+1 < len(keys) and time+1 != keys[i+1]:
+			keys.insert(i+1,time+1)
+			ty.insert(i+1,ty[i])
+		time=time+1
+		i=i+1
+	tmp=keys[-1]+1
+	keys = keys+[i for i in range(tmp,latest+1)]
+	ty = ty+[ty[-1] for i in range(tmp,latest+1)]
+	return keys,ty
 
 def ParsePaperTxt():
 	with open(name=unicode(paperPath,'utf8'),mode="rU") as f:
@@ -87,7 +113,7 @@ def Work():
 				out.write("%s\t%d\n"%(row[0], A))
 			out.write("</task3>\n")
 
-def SaveTrainResult():
+def saveTrainResult():
 	with open(name=unicode(trainPath,'utf8'),mode="r") as trainf:
 		reader=csv.reader(trainf)
 		first=True
@@ -111,7 +137,7 @@ def SaveTrainResult():
 					A = int(yp[0])
 				outf.write("%r\t%r\t%r\t%r\t%r\t\n"%(row[0],X,y,row[1],A))
 
-def Analsis():
+def analsis():
 	with open(name=unicode(tempPath,'utf8'),mode="r") as outf:
 		for row in outf:
 			dom = row.split('\t')
@@ -129,13 +155,68 @@ def Analsis():
 			plt.legend()
 			plt.show()
 
+def TrainAndValidation():
+	readerCount=0
+	train={}
+	with open(name=unicode(trainPath,'utf8'),mode="r") as trainf:
+		reader=csv.reader(trainf)
+		first=True
+		for row in reader:
+			if first:
+				first=False
+				continue
+			readerCount=readerCount+1
+			train[row[0]]=int(row[1])
+
+	X=np.zeros( (readerCount, int(np.ceil((latest-earliest+1)/timespan*1.0) )+1 ),dtype=int)
+	y=np.array(train.values()).reshape(readerCount,)
+	I=0
+	for aut in train.keys():
+		res,total = GroupReferedPaperByYear(aut)
+		keys,ty=SortAutAndReferred(res)
+		row=np.zeros((1,int(np.ceil((latest-earliest+1)/timespan*1.0))+1),dtype=int)
+		if len(keys)>0:
+			keys,ty = CompleteReferred(keys,ty)
+			if(len(keys)!=len(ty)):
+				print ("%r\n%r\n%r")%(aut,keys,ty)
+			for i in range(len(keys)):
+				row[0, (keys[i]-earliest)/timespan ]=row[0,(keys[i]-earliest)/timespan ]+ty[i]
+		X[I,:]=row
+		I=I+1
+	svr=Svr(kernel='linear');
+	svr.train(X,y)
+
+	with open(name=unicode(validationPath,'utf8'),mode="r") as csvf:
+		with open(name=unicode(output3Path,'utf8'),mode="w") as out:
+			reader=csv.reader(csvf)
+			firstRow=True
+			for row in reader:
+				if firstRow:
+					firstRow=False
+					out.write("<task3>\nauthorname\tcitation\n")
+					continue
+				row=np.zeros((1,int(np.ceil((latest-earliest+1)/timespan*1.0))+1),dtype=int)
+				res,total = GroupReferedPaperByYear(row[0])
+				keys,ty = SortAutAndReferred(res)
+				#todo 一起预测
+				if len(keys)>0:
+					keys,ty = CompleteReferred(keys,ty)
+					for i in range(len(keys)):
+						row[ 0,(keys[i]-earliest)/timespan ]=row[0, (keys[i]-earliest)/timespan ]+ty[i]
+				A=svr.Predict(row)
+				out.write("%s\t%d\n"%(row[0], A))
+			out.write("</task3>\n")
+
 
 def main():
-	#ParsePaperTxt()
-	#Test()
+	ParsePaperTxt()
 	#Work()
-	#SaveTrainResult()
-	Analsis()
+	#test()
+	#saveTrainResult()
+	#analsis()
+	TrainAndValidation()
+
+
 
 if __name__ == "__main__":
 	main()
