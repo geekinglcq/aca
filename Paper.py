beb056d7dcf1a6db861adb686bc81119bf4f1297
legend <Legend94rz@gmail.com>
2017-08-21 00:20:36 +0800

    task3 PA 0.52

--- Paper.py
+++ Paper.py
@@ -1,33 +1,32 @@
 class Paper(object):
-	"""description of class"""
-	__wholeData = {}
-	__authorData={}
-	def __init__(self,index,author="",title="",time=0,journal=""):
-		self.Index=index
-		self.Author=author
-		self.Title=title
-		self.Time=time
-		self.Journal=journal
-		self.References=[]
-		self.Referenced=[]
+    """description of class"""
+    __wholeData = {}
+    __authorData = {}
 
-	@staticmethod
-	def getPaperById(id):
-		if not (id in Paper.__wholeData):
-			Paper.__wholeData[id]=Paper(id)
-		return Paper.__wholeData[id]
+    def __init__(self, index, author="", title="", time=0, journal=""):
+        self.Index = index
+        self.Author = author
+        self.Title = title
+        self.Time = time
+        self.Journal = journal
+        self.References = []
+        self.Referenced = []
 
-	@staticmethod
-	def getPaperByAut(author):
-		if not (author in Paper.__authorData):
-			Paper.__authorData[author]=[]
-		return Paper.__authorData[author]
+    @staticmethod
+    def getPaperById(id):
+        if not (id in Paper.__wholeData):
+            Paper.__wholeData[id] = Paper(id)
+        return Paper.__wholeData[id]
 
-	@staticmethod
-	def addAutPaper(aut,pa):
-		if not (aut in Paper.__authorData):
-			Paper.__authorData[aut]=[pa]
-		else:
-			Paper.__authorData[aut].append(pa)
+    @staticmethod
+    def getPaperByAut(author):
+        if not (author in Paper.__authorData):
+            Paper.__authorData[author] = []
+        return Paper.__authorData[author]
 
-	
+    @staticmethod
+    def addAutPaper(aut, pa):
+        if not (aut in Paper.__authorData):
+            Paper.__authorData[aut] = [pa]
+        else:
+            Paper.__authorData[aut].append(pa)
--- task3.py
+++ task3.py
@@ -1,174 +1,146 @@
- #coding:utf-8
-from pylab import *
+# coding:utf-8
 import Paper
 import csv
-from ILearner import Svr
-import numpy as np
-import ast
-import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
-import random
+from ILearner import SparsePA
+from sklearn.model_selection import train_test_split
+
 
 # 文件路径
 paperPath = "F:\\ACAData\\task3\\papers.txt"
 trainPath = "F:\\ACAData\\task3\\train.csv"
-validationPath="F:\\ACAData\\task3\\validation.csv"
-output3Path="F:\\ACAData\\task3\\output3.txt"
+validationPath = "F:\\ACAData\\task3\\validation.csv"
+output3Path = "F:\\ACAData\\task3\\output3.txt"
 
-tempPath="F:\\ACAData\\task3\\temp3.txt"
+tempPath = "F:\\ACAData\\task3\\temp3.txt"
 
-cited={}
-map=Svr('linear')
+
+trainX = []
+trainY = []
+testX = []
+epsilon = 0.7
+C = 1
+
 
 def GroupReferedPaperByYear(author):
-	res={}
-	total=0
-	papers = Paper.Paper.getPaperByAut(author)
-	for paper in papers:
-		for refed in paper.Referenced:
-			total=total+1
-			if refed.Time in res:
-				res[refed.Time].append(refed)
-			else:
-				res[refed.Time] = [refed]
-	return res,total
+    res = {}
+    total = 0
+    papers = Paper.Paper.getPaperByAut(author)
+    for paper in papers:
+        for refed in paper.Referenced:
+            total = total + 1
+            if refed.Time in res:
+                res[refed.Time].append(refed)
+            else:
+                res[refed.Time] = [refed]
+    return res, total
 
-def GroupReferedPaper(id):
-	res={}
-	paper=Paper.Paper.getPaperById(id)
-	for refed in paper.Referenced:
-		if refed.Time in res:
-			res[refed.Time].append(refed)
-		else:
-			res[refed.Time]=[refed]
-	return res
-
-def SortAutAndReferred(res):
-	keys=res.keys()
-	keys.sort()
-	ty=[len(res[k]) for k in keys]
-	return keys,ty
 
 def ParsePaperTxt():
-	with open(paperPath,"r") as f:
-		for eachLine in f:
-			if eachLine.startswith('#index'):
-				i = int(eachLine[6:])
-				p=Paper.Paper.getPaperById(i)
-			elif eachLine.startswith("#@"):
-				p.Author = eachLine[2:-1].split(',')
-				for aut in p.Author:
-					Paper.Paper.addAutPaper(aut,p)
-			elif eachLine.startswith("#*"):
-				p.Title = eachLine[2:-1]
-			elif eachLine.startswith("#t"):
-				p.Time = int(eachLine[2:-1])
-			elif eachLine.startswith("#c"):
-				p.Journal = eachLine[2:-1]
-			elif eachLine.startswith("#%"):
-				t = Paper.Paper.getPaperById(int(eachLine[2:-1]))
-				p.References.append(t)
-				t.Referenced.append(p)
-			else:
-				pass
-def Train():
-	Id = 0
-	j=0
-	color=['b','g','r','c','m']
-	while Id in Paper.Paper._Paper__wholeData:
-		res = GroupReferedPaper(Id)
-		X = res.keys()
-		if len(X)>0:
-			X.sort()
-			ty = [len(res[k]) for k in X]
-			y=[sum(ty[0:i+1]) for i in range(len(X))]
-			#plt.plot(X,y,color[j%5],label=str(Id))
+    with open(paperPath, "r") as f:
+        for eachLine in f:
+            if eachLine.startswith('#index'):
+                i = int(eachLine[6:])
+                p = Paper.Paper.getPaperById(i)
+            elif eachLine.startswith("#@"):
+                p.Author = eachLine[2:-1].split(',')
+                for aut in p.Author:
+                    Paper.Paper.addAutPaper(aut, p)
+            elif eachLine.startswith("#*"):
+                p.Title = eachLine[2:-1]
+            elif eachLine.startswith("#t"):
+                p.Time = int(eachLine[2:-1])
+            elif eachLine.startswith("#c"):
+                p.Journal = eachLine[2:-1]
+            elif eachLine.startswith("#%"):
+                t = Paper.Paper.getPaperById(int(eachLine[2:-1]))
+                p.References.append(t)
+                t.Referenced.append(p)
+            else:
+                pass
 
-			if len(X)>2:
-				xp=X[-3:]
-				yp=y[-3:]
-			else:
-				xp=X
-				yp=y
-			svr = Svr('linear')
-			svr.train(  np.array(xp).reshape(len(xp),1),  np.array( yp ).reshape(len(yp),)  )
-			xp.append(2017)
-			yp = svr.predict(np.array(xp).reshape(len(xp),1))
-			cited[Id]=int(yp[-1])
-			#plt.plot(xp,yp,color[j%5]+'--')
-			#j=j+1
-			#if j%5==0:
-			#	plt.legend()
-			#	plt.show()
-		else:
-			cited[Id]=0
-		Id=Id+1
-		
-	with open(name=trainPath,mode="r") as csvf:
-		with open(name=tempPath,mode="w") as outf:
-			reader=csv.reader(csvf)
-			firstRow=True
-			for row in reader:
-				if firstRow:
-					firstRow=False
-					continue
-				papers = Paper.Paper.getPaperByAut(row[0])
-				s=0
-				for paper in papers:
-					s=s+cited[paper.Index]
-				if s!=0:
-					outf.write("%r,%r\n"%(s, int(row[1])/(s*1.0) ))
-				else:
-					outf.write("%r,0\n"% (s))
 
-def analysis():
-	global map
-	d={}
-	with open(name=tempPath,mode="r") as fin:
-		for line in fin:
-			l=line.split(',')
-			#if int(l[0]) in d:
-			#	print("warning: %r already in d, old value is: %r was substituded by: %r\n"%(int(l[0]),d[int(l[0])],int(l[1])))
-			d[int(l[0])]=float(l[1])
-	X=d.keys()
-	X.sort()
-	y=[d[k] for k in X]
-	map=Svr('linear')
-	map.train(np.array(X).reshape(len(X),1),  np.array( y ).reshape(len(y),))
-	Xp = range(X[0],X[-1]+1)
-	yp = map.predict(np.array(Xp).reshape(-1,1))
-	plt.plot(X,y,Xp,yp)
-	plt.show()
+def ReadTrain():
+    with open(name=trainPath, mode="r") as csvf:
+        reader = csv.reader(csvf)
+        firstRow = True
+        for row in reader:
+            if firstRow:
+                firstRow = False
+                continue
+            trainX.append(row[0])
+            trainY.append(int(row[1]))
 
-def Validation():
-	with open(name=validationPath,mode="r") as csvf:
-		with open(name=output3Path,mode="w") as out:
-			reader=csv.reader(csvf)
-			firstRow=True
-			for row in reader:
-				if firstRow:
-					firstRow=False
-					out.write("<task3>\nauthorname\tcitation\n")
-					continue
-				papers = Paper.Paper.getPaperByAut(row[0])
-				s=0
-				for paper in papers:
-					s=s+cited[paper.Index]
-				#yp=map.predict([[s]])
-				#A=yp[0]
-				#if A<0:
-				#	A=s
-				out.write("%s\t%d\n"%(row[0], s*7))
-			out.write("</task3>\n")
+
+def ReadValidation():
+    with open(name=validationPath, mode="r") as csvf:
+        reader = csv.reader(csvf)
+        firstRow = True
+        for row in reader:
+            if firstRow:
+                firstRow = False
+                continue
+            testX.append(row[0])
+
+
+def SelectModel():
+    X_train, X_test, y_train, y_test = train_test_split(
+        trainX, trainY, test_size=0.3, random_state=0)
+    C = [1e5,1e6,1e7]
+    opt = 0
+    for c in C:
+        m = SparsePA(c, 5)
+        m.train(X_train, y_train)
+        mape = m.MAPEScore(X_test, y_test)
+        print("C: %r,  MAPE: %r, train: %r,test: %r\n" % (c, mape,len(X_train),len(X_test)))
+        if mape > opt:
+            opt = mape
+            model = m
+    return model
+
+
+def GenResult(model):
+    with open(name=output3Path, mode="w") as out:
+        out.write("<task3>\nauthorname\tcitation\n")
+        Yp = model.predict(testX)
+        for i in range(len(Yp)):
+            out.write("%s\t%d\n" % (testX[i], Yp[i]))
+        out.write("</task3>\n")
+
+
+def analisis():
+    #找出哪些文章的被引的确为0
+    z=set()
+    o=set()
+    data={}
+    for i in range(len(trainX)):
+        data[trainX[i]]=trainY[i]
+        if trainY[i]==0:
+            aut=trainX[i]
+            papers=Paper.Paper.getPaperByAut(aut)
+            for paper in papers:
+                z.add(paper)
+    for paper in z:
+        for aut in paper.Author:
+            if (aut in data) and data[aut]>0:
+                o.add(paper)
+    z.difference_update(o)
+    with (open('zo.txt',mode='w')) as fout:
+        fout.write("%r\n"%len(z))
+        for p in z:
+            fout.write("%r,%r\n"%(p.Time,p.Journal))
+        fout.write("%r\n"%len(o))
+        for p in o:
+            fout.write("%r,%r\n"%(p.Time,p.Journal))
+            
 
 def main():
-	ParsePaperTxt()
-	Train()
-	#analysis()
-	Validation()
-
+    ParsePaperTxt()
+    ReadTrain()
+    #analisis()
+    ReadValidation()
+    model = SelectModel()
+    GenResult(model)
 
 
 if __name__ == "__main__":
-	main()
-
+    main()
