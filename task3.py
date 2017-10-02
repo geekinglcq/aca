9fb80a24d2c515e649f61ca79704c9265920f19f
legend <Legend94rz@gmail.com>
2017-10-02 18:00:58 +0800

    task3 local validation 0.537。5it, 0.7train

--- task3.py
+++ task3.py
@@ -1,10 +1,8 @@
 # coding:utf-8
 import Paper
 import csv
-import ILearner
+from ILearner import SparsePA
 from sklearn.model_selection import train_test_split
-import datetime
-import pickle
 
 
 # 文件路径
@@ -13,13 +11,32 @@ trainPath = "F:\\ACAData\\task3\\train.csv"
 validationPath = "F:\\ACAData\\task3\\validation.csv"
 output3Path = "F:\\ACAData\\task3\\output3.txt"
 
+tempPath = "F:\\ACAData\\task3\\temp3.txt"
+
+
 trainX = []
 trainY = []
 testX = []
+epsilon = 0.7
+C = 1
+
+
+def GroupReferedPaperByYear(author):
+    res = {}
+    total = 0
+    papers = Paper.Paper.getPaperByAut(author)
+    for paper in papers:
+        for refed in paper.Referenced:
+            total = total + 1
+            if refed.Time in res:
+                res[refed.Time].append(refed)
+            else:
+                res[refed.Time] = [refed]
+    return res, total
+
 
 def ParsePaperTxt():
-    print("%s parse paper"%datetime.datetime.now())
-    with open(paperPath, "r",encoding='utf-8') as f:
+    with open(paperPath, "r") as f:
         for eachLine in f:
             if eachLine.startswith('#index'):
                 i = int(eachLine[6:])
@@ -43,8 +60,7 @@ def ParsePaperTxt():
 
 
 def ReadTrain():
-    print("%s read training set"%datetime.datetime.now())
-    with open(trainPath, "r",encoding='utf-8') as csvf:
+    with open(name=trainPath, mode="r") as csvf:
         reader = csv.reader(csvf)
         firstRow = True
         for row in reader:
@@ -56,8 +72,7 @@ def ReadTrain():
 
 
 def ReadValidation():
-    print("%s read validation set"%datetime.datetime.now())
-    with open(validationPath, "r",encoding='utf-8') as csvf:
+    with open(name=validationPath, mode="r") as csvf:
         reader = csv.reader(csvf)
         firstRow = True
         for row in reader:
@@ -68,25 +83,23 @@ def ReadValidation():
 
 
 def SelectModel():
-    print("%s select model"%datetime.datetime.now())
     X_train, X_test, y_train, y_test = train_test_split(
         trainX, trainY, test_size=0.3, random_state=0)
-    c=0
-    #m = SparsePA(c, 1000)
-    m = ILearner.DNN()
-    m.train(trainX, trainY)
-    mape = m.score(X_test, y_test)
-    print("C: %r,  MAPE: %r, train: %r, test: %r\n" %
-          (c, mape, len(trainY), len(y_test)))
-    if mape > opt:
-        opt = mape
-        model = m
+    C = [0]
+    opt = 0
+    for c in C:
+        m = SparsePA(c, 5)
+        m.train(X_train, y_train)
+        mape = m.MAPEScore(X_test, y_test)
+        print("C: %r,  MAPE: %r, train: %r,test: %r\n" % (c, mape,len(X_train),len(X_test)))
+        if mape > opt:
+            opt = mape
+            model = m
     return model
 
 
 def GenResult(model):
-    print("%s save model"%datetime.datetime.now())
-    with open(output3Path, "w",encoding='utf-8') as out:
+    with open(name=output3Path, mode="w") as out:
         out.write("<task3>\nauthorname\tcitation\n")
         Yp = model.predict(testX)
         for i in range(len(Yp)):
@@ -95,30 +108,39 @@ def GenResult(model):
 
 
 def analisis():
-    tempPath = "F:\\ACAData\\task3\\"
-    with open(tempPath+"temp.txt",'w',encoding='utf-8') as fout:
-        fout.write("%r\n"%len(trainX))
-        for i in range(len(trainX)):
-            papers = Paper.Paper.getPaperByAut(trainX[i])
-            A = [(papers[j].Index, len(papers[j].Referenced)) for j in range(len(papers))]
-            for j in range(len(A)):
-                fout.write("%d:%d;"%(A[j][0],A[j][1]))
-            fout.write("%d\n"%trainY[i])
-
+    #找出哪些文章的被引的确为0
+    z=set()
+    o=set()
+    data={}
+    for i in range(len(trainX)):
+        data[trainX[i]]=trainY[i]
+        if trainY[i]==0:
+            aut=trainX[i]
+            papers=Paper.Paper.getPaperByAut(aut)
+            for paper in papers:
+                z.add(paper)
+    for paper in z:
+        for aut in paper.Author:
+            if (aut in data) and data[aut]>0:
+                o.add(paper)
+    z.difference_update(o)
+    with (open('zo.txt',mode='w')) as fout:
+        fout.write("%r\n"%len(z))
+        for p in z:
+            fout.write("%r,%r\n"%(p.Time,p.Journal))
+        fout.write("%r\n"%len(o))
+        for p in o:
+            fout.write("%r,%r\n"%(p.Time,p.Journal))
+            
 
 def main():
     ParsePaperTxt()
-    #Paper.Paper.MergerPaper()
     ReadTrain()
-    analisis()
+    #analisis()
     ReadValidation()
     model = SelectModel()
-    model.save('optModel.txt')
     GenResult(model)
-
-
 
 
 if __name__ == "__main__":
     main()
-
